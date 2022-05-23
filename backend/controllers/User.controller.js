68855064174cb9ed41d24a6a0ce04b6d12203d9e
Korootak <jordanwidmer78@gmail.com>
2022-05-23 14:36:49 +0200

    Modification Backend

--- backend/controllers/User.controller.js
+++ backend/controllers/User.controller.js
@@ -22,7 +22,7 @@ const pool = mysql.createPool({
   connectionLimit: 100,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: "",
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 });
